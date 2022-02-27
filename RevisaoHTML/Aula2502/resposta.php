62e62619e280dff172e415414edc5104626bff73
BellaLima <isabela.lima2@fatec.sp.gov.br>
2022-02-27 17:39:21 -0300

    Update resposta.php

--- RevisaoHTML/Aula2502/resposta.php
+++ RevisaoHTML/Aula2502/resposta.php
@@ -17,6 +17,10 @@
        $Valor1 = $_POST['Valor1'];
        $Valor2 = $_POST['Valor2'];
        echo "Valor 1 é: $Valor1 e Valor 2 é: $Valor2";
+       if (($Valor1 % 2) == 0);
+       echo "Valor 1 é Par"
+       else
+       echo "Valor 1 é impar"
        //mais menos vezes e dividir
 
     ?>
