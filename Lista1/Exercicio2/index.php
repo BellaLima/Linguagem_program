fdd750420a43a9e037c10e6f78c510f3d498fd80
BellaLima <isabela.lima2@fatec.sp.gov.br>
2022-03-16 20:37:44 -0300

    comentar

--- Lista1/Exercicio2/index.php
+++ Lista1/Exercicio2/index.php
@@ -8,29 +8,19 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Exercicio1 Resposta</title>
+    <title>Exercicio 2</title>
   </head>
   <body class="container">
-    <h1>Exercicio1 Resposta</h1>
-    <?php
-           for ($i= 1; $i<=10; $i++){
-               $vetor[$i] = $_POST["valor$i"];
-           }
-           $posiscao = 0;
-           $maior = 0;
+    <h1>Exercicio 2</h1>
+    <div class="row">
+        <div class="col">
             
-           //var_dump($vetor);
-           foreach($vetor as $chave => $valor){
-               echo "Posição do vetor: $chave e seu valor é $valor";
-               echo "<br>";
-               if ($valor > $maior){
-                   $maior = $valor;
-                   $posiscao = $chave;
-               }
-           }
-           echo "O maior valor é $maior e está na posição $posiscao"
-    ?>
+        </div>
+    </div>
 
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
   </body>
 </html>
