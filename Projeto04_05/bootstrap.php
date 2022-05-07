e9ff4e3604191a4a0059554084e3f7de9b81aa54
BellaLima <93015684+BellaLima@users.noreply.github.com>
2022-05-06 21:31:07 -0300

    Update bootstrap.php

--- Projeto04_05/bootstrap.php
+++ Projeto04_05/bootstrap.php
@@ -10,7 +10,8 @@ $path = $_SERVER['PATH_INFO'];
 $router = new \Aluno\Projeto0405\Router($method, $path);
 
 $router->get('/ola-mundo', function(){
-    return 'ola mundo! <br> ola Isabela <br> Ola chico <br> ola Emerson';
+    return 'ola mundo! <br> ola Isabela <br> Ola chico <br> ola Emerson <br>
+    O Brasil registrou nesta sexta-feira (6) 176 mortes pela Covid-19 nas últimas 24 horas, totalizando 664.143 desde o início da pandemia. Com isso, a média móvel de mortes nos últimos 7 dias é de 94. Em comparação à média de 14 dias atrás, a variação foi de -2%, indicando tendência de estabilidade nos óbitos decorrentes da doença.';
 });
 
 $result = $router->handler();
