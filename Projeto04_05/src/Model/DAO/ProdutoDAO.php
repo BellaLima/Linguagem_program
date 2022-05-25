7808d54fe58899bf566a0fc672f20f6f8b1c158f
BellaLima <93015684+BellaLima@users.noreply.github.com>
2022-05-25 19:37:18 -0300

    Update ProdutoDAO.php

---
+++ Projeto04_05/src/Model/DAO/ProdutoDAO.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aluno\Projeto0405\Model\DAO;
+
+class ProdutoDAO{
+
+    public function inserir(){}
+
+    public function alterar(){}
+
+    public function excluir(){}
+
+    public function consultar(){}
+    
+}
